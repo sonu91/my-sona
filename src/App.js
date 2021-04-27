cfb9da7fa2eac293f9082f4627899de4a41b38bb
Jha <uidv5304@contiwan.com>
2021-04-27 19:11:17 +0530

    my cmt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import DataGrid from 'react-data-grid';
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'name', name: 'Name' },
+  { key: 'customer', name: 'Customer' },
+  { key: 'productGroup', name: 'ProductGroup' },
+  { key: 'status', name: 'Status' },
+  { key: 'locationSE', name: 'Location SE' },
+  { key: 'locationTE', name: 'Location TE' }
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+/**
+ * App
+ *
+ * Simple react js fetch example
+ */
+class App extends React.Component {
+
+    /**
+     * constructor
+     *
+     * @object  @props  parent props
+     * @object  @state  component state
+     */
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    /**
+     * componentDidMount
+     *
+     * Fetch json array of objects from given url and update state.
+     */
+    componentDidMount() {
+
+        fetch('http://10.215.241.161:8087/api/project') /* http://10.215.241.161:81/angapp/     http://10.215.241.161:8087/api/project*/
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+    /**
+     * render
+     *
+     * Render UI
+     */
+    render() {
+
+        const { isLoaded, items } = this.state;
+
+        if (!isLoaded)
+            return <div>Loading Trek Project Details...Please wait</div>;
+
+        return (
+            <div className="App">
+                        <DataGrid                      
+                        columns={columns}
+                        rows={items}  />
+              </div>
+        );
+
+    }
+
 }
-
 export default App;
